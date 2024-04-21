ccd26e9ce099e3cb6cdb96a807e6e0f13ec6ed62
LQC <lqcuong99@gmail.com>
2024-04-21 10:39:24 +0700

    init nlp - tokenizer

---
+++ thesis-nlp/src/main/java/thesis/POSTagger/POSTagger.java
@@ -0,0 +1,4 @@
+package thesis.POSTagger;
+
+public class POSTagger {
+}
---
+++ thesis-nlp/src/main/java/thesis/ThesisNlpApplication.java
@@ -0,0 +1,21 @@
+package thesis;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+import java.util.List;
+import java.util.Properties;
+import java.util.zip.GZIPInputStream;
+
+@SpringBootApplication
+public class ThesisNlpApplication {
+
+    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(ThesisNlpApplication.class, args);
+    }
+}
