3576847dbd045e0adc1afcf04426565e5b7106ba
LQC <lqcuong99@gmail.com>
2024-05-04 21:09:35 +0700

    update

--- thesis-core/src/main/java/thesis/core/search_engine/SearchEngine.java
+++ thesis-core/src/main/java/thesis/core/search_engine/SearchEngine.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import thesis.core.article.command.CommandCommonQuery;
+import thesis.core.article.model.author.Author;
 import thesis.core.article.model.author.service.AuthorService;
 import thesis.core.article.model.label.custom_label.CustomLabel;
 import thesis.core.article.model.label.custom_label.service.CustomLabelService;
@@ -16,7 +17,9 @@ import thesis.core.article.model.label.org_label.ORGLabel;
 import thesis.core.article.model.label.org_label.service.ORGLabelService;
 import thesis.core.article.model.label.per_label.PERLabel;
 import thesis.core.article.model.label.per_label.service.PERLabelService;
+import thesis.core.article.model.location.Location;
 import thesis.core.article.model.location.service.LocationService;
+import thesis.core.article.model.topic.Topic;
 import thesis.core.article.model.topic.service.TopicService;
 
 import java.util.Map;
@@ -26,6 +29,9 @@ import java.util.stream.Collectors;
 @Getter
 @Log4j2
 public class SearchEngine {
+    private Map<String, Author> authorMap;
+    private Map<String, Topic> topicMap;
+    private Map<String, Location> locationMap;
     private Map<String, CustomLabel> customLabelMap;
     private Map<String, NLPLabel> nlpLabelMap;
     private Map<String, PERLabel> perLabelMap;
@@ -42,6 +48,18 @@ public class SearchEngine {
                       PERLabelService perLabelService,
                       LOCLabelService locLabelService) {
         log.info("=== start init label maps");
+        authorMap = authorService.getMany(CommandCommonQuery.builder()
+                .page(1)
+                .size(Integer.MAX_VALUE)
+                .build()).stream().collect(Collectors.toMap(Author::getAuthor, author -> author));
+        topicMap = topicService.getMany(CommandCommonQuery.builder()
+                .page(1)
+                .size(Integer.MAX_VALUE)
+                .build()).stream().collect(Collectors.toMap(Topic::getTopic, topic -> topic));
+        locationMap = locationService.getMany(CommandCommonQuery.builder()
+                .page(1)
+                .size(Integer.MAX_VALUE)
+                .build()).stream().collect(Collectors.toMap(Location::getLocation, location -> location));
         customLabelMap = customLabelService.getMany(CommandCommonQuery.builder()
                 .page(1)
                 .size(Integer.MAX_VALUE)
